38367f248ba466186f351294ae4d396d9012afc7
Steve Finck <stevefinck@gmail.com>
2022-05-02 09:09:33 -0500

    build out categories and add restaurant item

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import {View, Text, SafeAreaView} from "react-native";
+import {View, Text, SafeAreaView, ScrollView} from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
+import RestaurantItem from "../components/RestaurantItem";
 
 export default function Home() {
   return (
@@ -11,7 +12,12 @@ export default function Home() {
         <HeaderTabs/>
         <SearchBar/>
       </View>
-      <Categories/>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories/>
+        <RestaurantItem />
+        <RestaurantItem />
+        <RestaurantItem />
+      </ScrollView>
     </SafeAreaView>
   );
 }
